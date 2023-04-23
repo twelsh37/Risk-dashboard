4a761f4dc3d505c9df9c84bafdbb55c3c125eaf6
Tom Welsh <4956770+twelsh37@users.noreply.github.com>
2023-04-23 19:46:43 +0100

    added  dbctheme

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ controlb = cb.value_counts()
 # Dash App below
 #------------------------------------------------------------------------------
 # Set Dash app title
-app = dash.Dash(__name__, title='RACA Analytics')
+app = dash.Dash(__name__, title='RACA Analytics', external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 cards = go.Figure()
 
@@ -30,7 +30,7 @@ cards.add_trace(go.Indicator(
     delta={'reference': a_process},
     domain={'row': 1, 'column': 0},
     title={"text": "<b>Processes</b>", 'font': {'size': 24}},
-    number={"font": {"size": 60}},
+    number={"font": {"size": 48}},
         )
     )
 
@@ -38,9 +38,9 @@ cards.add_trace(go.Indicator(
     mode="number+delta",
     value=b_risk,
     delta={'reference': a_risk},
-    domain={'row': 1, 'column': 1},
+    domain={'row': 1, 'column':2},
     title={"text": "<b>Risks</b>", 'font': {'size': 24}},
-    number={"font": {"size": 60}},
+    number={"font": {"size": 48}},
         )
     )
 
@@ -48,9 +48,9 @@ cards.add_trace(go.Indicator(
     mode="number+delta",
     value=b_controls,
     delta={'reference':a_controls},
-    domain={'row': 1, 'column': 2},
+    domain={'row': 1, 'column': 4},
     title={"text": "<b>Controls</b>", 'font': {'size': 24}},
-    number={"font": {"size": 60}},
+    number={"font": {"size": 48}},
         )
     )
 
@@ -58,9 +58,9 @@ cards.add_trace(go.Indicator(
     mode="number+delta",
     value=b_actions,
     delta={'reference': a_actions},
-    domain={'row': 1, 'column': 3},
+    domain={'row': 1, 'column': 6},
     title={"text": "<b>Actions</b>", 'font': {'size': 24}},
-    number={"font": {"size": 60}},
+    number={"font": {"size": 48}},
         )
     )
 
@@ -68,7 +68,7 @@ control = go.Figure(go.Indicator(
     mode="gauge+number+delta",
     value=controla[0],
     domain={'x': [0, 1], 'y': [0, 1]},
-    title={'text': "Manual or Automatic", 'font': {'size': 24}},
+    title={'text': "<b>Manual or Automatic<br>Controls<br> </b>", 'font': {'size': 24}},
     delta={'reference': controlb[0], 'increasing': {'color': "RebeccaPurple"}},
     gauge={
         'axis': {'range': [None, 1500], 'tickwidth': 1, 'tickcolor': "darkblue"},
@@ -109,20 +109,16 @@ app.layout = dbc.Container([html.Div([
         dbc.Col([html.H4("General Numbers", style={"width": "100%"})], width=6),
     ]),
     dbc.Row([
-        dbc.Col([dcc.Graph(id='stats', figure=cards)], className='three columns', width=3),
-        dbc.Col([dcc.Graph(id='stats2', figure=cards)], className='three columns', width=3),
-        dbc.Col([dcc.Graph(id='stats3', figure=cards)], className='three columns', width=3),
-        # dbc.Col([dcc.Graph(id='control1', figure=control)], className='three columns', width=3),
-        # dbc.Col([dcc.Graph(id='control2', figure=control)], className='three columns', width=3),
+        dbc.Col([dcc.Graph(id='stats', figure=cards)], className='three columns', width=6),
+        dbc.Col([dcc.Graph(id='control1', figure=control)], className='three columns', width=3),
+        dbc.Col([dcc.Graph(id='control2', figure=control)], className='three columns', width=3),
     ], className='row'),
+    html.Hr(),
 # dbc.Row([
-#         dbc.Col([dcc.Graph(id='stats', figure=cards)], width=6),
-#         dbc.Col([dcc.Graph(id='control', figure=control)], width=3),
-#         dbc.Col([dcc.Graph(id='control', figure=control)], width=3),
+#         dbc.Col([dcc.Graph(id='stats2', figure=cards)], width=6),
 #     ]),
-
 ]),
-])
+], fluid=True)
 
 app.run_server(debug=True)
 
