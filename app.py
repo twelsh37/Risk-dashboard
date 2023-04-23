aa62c8669eecfb96e36e7335864b053ccd037491
Tom Welsh <4956770+twelsh37@users.noreply.github.com>
2023-04-23 23:06:14 +0100

    Updates to rows and addition of sunburst chart

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from dash import dcc
 from dash import html
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
+import plotly.express as px
 import pandas as pd
 
 # pull in functions from our Dataprep.py file
@@ -11,11 +12,13 @@ from dataprep import sanity_check
 
 
 # Run our sanity check function on each dataframe
-b_process, b_risk, b_controls, b_actions, a_process, a_risk, a_controls, a_actions, ca, cb = sanity_check()
+y2020raca, y2021raca, y2022raca, b_process, b_risk, b_controls, b_actions, a_process, a_risk, a_controls, a_actions, ca, cb\
+    = sanity_check()
 
 # get the data for our Control activity gauge
 controla = ca.value_counts()
 controlb = cb.value_counts()
+
 
 # Dash App below
 #------------------------------------------------------------------------------
@@ -28,7 +31,7 @@ cards.add_trace(go.Indicator(
     mode="number+delta",
     value=b_process,
     delta={'reference': a_process},
-    domain={'row': 1, 'column': 0},
+    domain={'row': 1, 'column': 1},
     title={"text": "<b>Processes</b>", 'font': {'size': 24}},
     number={"font": {"size": 48}},
         )
@@ -38,7 +41,7 @@ cards.add_trace(go.Indicator(
     mode="number+delta",
     value=b_risk,
     delta={'reference': a_risk},
-    domain={'row': 1, 'column':2},
+    domain={'row': 1, 'column':4},
     title={"text": "<b>Risks</b>", 'font': {'size': 24}},
     number={"font": {"size": 48}},
         )
@@ -48,7 +51,7 @@ cards.add_trace(go.Indicator(
     mode="number+delta",
     value=b_controls,
     delta={'reference':a_controls},
-    domain={'row': 1, 'column': 4},
+    domain={'row': 1, 'column': 7},
     title={"text": "<b>Controls</b>", 'font': {'size': 24}},
     number={"font": {"size": 48}},
         )
@@ -58,10 +61,18 @@ cards.add_trace(go.Indicator(
     mode="number+delta",
     value=b_actions,
     delta={'reference': a_actions},
-    domain={'row': 1, 'column': 6},
+    domain={'row': 1, 'column': 10},
     title={"text": "<b>Actions</b>", 'font': {'size': 24}},
     number={"font": {"size": 48}},
         )
+    )
+
+cards.update_layout(
+    grid={'rows': 1, 'columns': 12, 'pattern': "independent"},
+    template={'data': {'indicator': [{
+        'mode': "number+delta+gauge",
+        }],
+                  }}
     )
 
 control = go.Figure(go.Indicator(
@@ -72,52 +83,89 @@ control = go.Figure(go.Indicator(
     delta={'reference': controlb[0], 'increasing': {'color': "RebeccaPurple"}},
     gauge={
         'axis': {'range': [None, 1500], 'tickwidth': 1, 'tickcolor': "darkblue"},
-        'bar': {'color': "#008080"},
+        'bar': {'color': "#000080"},
         'bgcolor': "white",
-        'borderwidth': 2,
+        'borderwidth': 1,
         'bordercolor': "silver",
         'steps': [
-            {'range': [0, 750], 'color': '#808000'},
-            {'range': [750, 1500], 'color': '#800080'},
-
-        ],
+            {'range': [0, 1500], 'color': '#808000'},
+                   ],
 
     }))
 
+controlactivity = go.Figure()
+
+# Add the histogram traces to the figure
+for column_header in ['control_activity']:
+    controlactivity.add_trace(go.Histogram(x=y2020raca[column_header], name='2020', marker={'color': '#AA4639'}))
+    controlactivity.add_trace(go.Histogram(x=y2021raca[column_header], name='2021', marker = {'color': '#AA7139'}))
+    controlactivity.add_trace(go.Histogram(x=y2022raca[column_header], name='2022', marker = {'color': '#255C69'}))
+
+# See Plotly documentation for customizations: https://plotly.com/python/reference/histogram/
+controlactivity.update_layout(
+    xaxis_title='control_activity',
+    title='<b>Control Activity by Type<b>',
+    barmode='group',
+    plot_bgcolor='rgba(0,0,0,0)',
+    xaxis_linecolor = 'Black',
+    yaxis_linecolor = 'Black',
+    xaxis_categoryorder = "category ascending",
+    yaxis_title='Number'
+)
+
+de_oe = go.Figure()
 
 
-cards.update_layout(
-    grid={'rows': 1, 'columns': 12, 'pattern': "independent"},
-    template={'data': {'indicator': [{
-        'mode': "number+delta+gauge",
-        }],
-                  }}
-    )
+# Add the histogram traces to the figure
+for column_header in ['de_oe']:
+    de_oe.add_trace(go.Histogram(x=y2020raca[column_header], name='2020', marker = {'color': '#AA4639'}))
+    de_oe.add_trace(go.Histogram(x=y2021raca[column_header], name='2021', marker = {'color': '#AA7139'}))
+    de_oe.add_trace(go.Histogram(x=y2022raca[column_header], name='2022', marker = {'color': '#255C69'}))
+# Update the layout
+
+de_oe.update_layout(
+	xaxis_title='Do the Risk Owners regard the Controls Effective',
+    title='<b>Control Effectiveness?<b>',
+    barmode='group',
+    plot_bgcolor='rgba(0,0,0,0)',
+    xaxis_linecolor = 'Black',
+    yaxis_linecolor = 'Black',
+    xaxis_categoryorder = "category ascending",
+    yaxis_title='Number'
+)
+
+sun = px.sunburst(
+    data_frame=y2020raca,
+    # Lays out the sunburst from our L1, to L2, to level 3 risk categories
+    path=['risk_types', 'risk', 'level3'],
+    color_continuous_scale=px.colors.sequential.BuGn,
+    maxdepth=3,
+    branchvalues='total',  # other option is 'remainder'
+    hover_name='risk_types',
+    title='Breakdown of Risk by Risk Type',
+    template='presentation',
+)
+
+sun.update_layout(title_x=0.5, height=900, uniformtext=dict(minsize=10))
+sun.update_traces(textinfo='label+percent parent+value', insidetextorientation='radial')
 
 
 
-# control.update_layout(paper_bgcolor="lavender", font={'color': "darkblue", 'family': "Arial"})
 
 # Bootstrap it up
 # ------------------------------------------------------------------------------
 app.layout = dbc.Container([html.Div([
     html.Br(),
     dbc.Row([
-        dbc.Col([html.H1('Analytics Dashboard', style={"width": "100%"})], width=12),
-    ]),
-    dbc.Row([
-        dbc.Col([html.H4("General Numbers", style={"width": "100%"})], width=6),
+        dbc.Col([html.H1('RACA Dashboard', style={"width": "100%"})],  width=12),
     ]),
     dbc.Row([
         dbc.Col([dcc.Graph(id='stats', figure=cards)], className='three columns', width=6),
-        dbc.Col([dcc.Graph(id='control1', figure=control)], className='three columns', width=3),
-        dbc.Col([dcc.Graph(id='control2', figure=control)], className='three columns', width=3),
+        dbc.Col([dcc.Graph(id='control1', figure=controlactivity)], className='three columns', width=3),
+        dbc.Col([dcc.Graph(id='control2', figure=de_oe)], className='three columns', width=3),
     ], className='row'),
-    html.Hr(),
-# dbc.Row([
-#         dbc.Col([dcc.Graph(id='stats2', figure=cards)], width=6),
-#     ]),
-]),
+        dbc.Col([dcc.Graph(id='sunburst', figure=sun)], className='three columns', width=6),
+    ]),
 ], fluid=True)
 
 app.run_server(debug=True)
